cc09ed3d9b7b9dc36b194c3e75ad5560d5be702e
shuchaoG <1021473463@qq.com>
2017-06-14 16:24:39 +0800

    index login reg and pagedetail.html

--- manager.py
+++ manager.py
@@ -15,12 +15,15 @@ def init_database():
      db.drop_all()
      db.create_all()
      for i in range(0,10):
-          db.session.add(User('User'+str(i),'a'+str(i)))
+          db.session.add(User('User'+str(i+1),'a'+str(i+1)),)
           for j in range(0,3):
               db.session.add(Image(get_image_url(),i+1))
               for k in range(0, 3):
                    db.session.add(Comment('This is a comment' + str(k), 1 + 3 * i + j, i + 1))
      db.session.commit()
+
+     print 1,User.query.all()
+     print 2,User.query.paginate(page=1,per_page=3).items
 
 if __name__=='__main__':
      manager.run()
--- nowstagram/__init__.py
+++ nowstagram/__init__.py
@@ -2,11 +2,14 @@
 
 from flask import Flask
 from  flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app=Flask(__name__)
 app.config.from_pyfile('app.conf')
-
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+app.secret_key='nowcoder'
 db=SQLAlchemy(app)
-
+login_manager=LoginManager(app)
+login_manager.login_view='/regloginpage/'
 
 from nowstagram import views,models
