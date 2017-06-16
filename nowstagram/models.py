47339b2dfba24befc1e5402ab2374a8de07c80f9
shuchaoG <1021473463@qq.com>
2017-06-16 17:22:41 +0800

    some changes

--- nowstagram/models.py
+++ nowstagram/models.py
@@ -1,16 +1,16 @@
 # -*- encoding=UTF-8 -*-
 
-from nowstagram import db,login_manager
+from nowstagram import db, login_manager
 from datetime import datetime
 import random
 
 class Comment(db.Model):
-   # __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     content = db.Column(db.String(1024))
     image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    status = db.Column(db.Integer, default=0) # 0 正常 1 删除
+    status = db.Column(db.Integer, default=0) # 0 正常 1 被删除
     user = db.relationship('User')
 
     def __init__(self, content, image_id, user_id):
@@ -19,43 +19,41 @@ class Comment(db.Model):
         self.user_id = user_id
 
     def __repr__(self):
-        return ('<Comment %d %s>' % (self.id, self.content)).encode('gbk')
+        return '<Comment %d %s>' % (self.id, self.content)
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    url=db.Column(db.String(512))
-    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
-    creat_date=db.Column(db.DateTime)
+    url = db.Column(db.String(512))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_date = db.Column(db.DateTime)
     comments = db.relationship('Comment')
 
-    def __init__(self,url,use_id):
-        self.url=url
-        self.user_id=use_id
-        self.creat_date=datetime.now()
+    def __init__(self, url, user_id):
+        self.url = url
+        self.user_id = user_id
+        self.created_date = datetime.now()
 
     def __repr__(self):
         return '<Image %d %s>' % (self.id, self.url)
 
-
-
-
-
 class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    username=db.Column(db.String(80),unique=True)
-    password=db.Column(db.String(32))
-    salt=db.Column(db.String(32))
-    head_url=db.Column(db.String(256))
-    images=db.relationship('Image',backref='user',lazy='dynamic')
+    # __tablename__ = 'user'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(32))
+    salt = db.Column(db.String(32))
+    head_url = db.Column(db.String(256))
+    images = db.relationship('Image', backref='user', lazy='dynamic')
 
-    def __init__(self,username,password,salt=''):
-        self.username=username
-        self.password=password
-        self.salt=salt
-        self.head_url='http://images.nowcoder.com/head/'+str(random.randint(0,1000))+'m.png'
+    def __init__(self, username, password, salt=''):
+        self.username = username
+        self.password = password
+        self.salt = salt
+        self.head_url = 'http://images.nowcoder.com/head/' + str(random.randint(0,1000)) +  'm.png'
 
     def __repr__(self):
-        return '<User %d %s>' %(self.id,self.username)
+        return '[User %d %s]' % (self.id, self.username)
 
     @property
     def is_authenticated(self):
@@ -69,11 +67,10 @@ class User(db.Model):
     def is_anonymous(self):
         return False
 
-    @property
     def get_id(self):
         return self.id
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(load_user)
+    return User.query.get(user_id)
